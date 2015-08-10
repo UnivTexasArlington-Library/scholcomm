8cb25f60fdf02a14d3cdd3a276ba57e177f3f034
Priyadharshini132 <priya.guns@gmail.com>
2015-08-10 17:30:53 -0500

    Changes made to navigation and carousel

--- sites/all/themes/sc_bootstrap/js/menu-anim.js
+++ sites/all/themes/sc_bootstrap/js/menu-anim.js
@@ -1,16 +1,17 @@
 jQuery(document).ready(function($) {
-	/*add class to remove the border*/
+	// add class to remove the border
 	$("nav ul li").on("click", function(e) {
 		e.preventDefault();
 		if($(this).hasClass("nav-ul-li-remove-border")){
 			$(this).removeClass("nav-ul-li-remove-border");
 		}
 		else{
-			$(this).addClass("nav-ul-li-remove-border");	
+			$(this).addClass("nav-ul-li-remove-border");
+		
 		}
 	});
 
-	/*add calss to remove padding*/
+	//add class to remove padding
 	$("nav ul li a").on("click", function(e) {
 		e.preventDefault();
 		if($(this).hasClass("nav-ul-li-a-remove-padding")){
@@ -21,5 +22,18 @@ jQuery(document).ready(function($) {
 		}
 	});
 
+	/*$('html').click(function () {
+		if($("nav ul li").hasClass("nav-ul-li-remove-border")){
+			$("nav ul li").removeClass("nav-ul-li-remove-border");
+		}*/
+    /*if (e.target.id == 'li.first.expanded.dropdown.active') {
+        //do something
+        $("nav ul li").removeclass("nav-ul-li-remove-border");
+    } else {
+        //do something
+    }*/
+	/*});*/
 
 });
+
+
