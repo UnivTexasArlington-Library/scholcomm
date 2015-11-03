e77d6129fac1eb394b357cff9892d15bb717725a
kschenk <kschenk@uta.edu>
2015-11-03 16:19:51 -0600

    remove menu-anim.js

--- sites/all/themes/sc_bootstrap/template.php
+++ sites/all/themes/sc_bootstrap/template.php
@@ -4,7 +4,7 @@
  * @file
  * template.php
  */
-drupal_add_js(drupal_get_path('theme', 'sc_bootstrap'). '/js/menu-anim.js');
+drupal_add_js(drupal_get_path('theme', 'sc_bootstrap'));
 
 
 // function sc_bootstrap_subtheme_preprocess_html(&$variables) {
